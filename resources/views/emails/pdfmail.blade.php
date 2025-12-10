79a6dc4b9f0a04c56dceb1204212d456f714d557
sunny <martaroiloa05@gmail.com>
2025-12-10 17:55:43 +0200

    PDF generation(student)2.1

--- resources/views/emails/pdfmail.blade.php
+++ resources/views/emails/pdfmail.blade.php
@@ -7,9 +7,3 @@
     <p>We are attaching the PDF document you generated.</p>
 </body>
 </html>
-
-email sending mail from the controller:
-use Illuminate\Support\Facades\Mail;
-use App\Mail\PDFMail;
-
-Mail::to('test@example.com')->send(new PDFMail());
